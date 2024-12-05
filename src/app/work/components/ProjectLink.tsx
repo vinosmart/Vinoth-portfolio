26527280e7fa9419a7908a41a461602f15880eac
Vinothkumar.M <102602070+vinosmart@users.noreply.github.com>
2024-12-05 15:33:46 +0530

    feat:add kodestory

--- src/app/work/components/ProjectLink.tsx
+++ src/app/work/components/ProjectLink.tsx
@@ -4,7 +4,7 @@ const ProjectSection = () => {
   const projects = [
     { title: 'Lmes', link: 'https://lmes.in/' },
     { title: 'Chitti', link: 'https://chitti.app/' },
-    { title: 'Fly', link: 'https://app.fly-social.com/' },
+    { title: 'Fly', link: 'https://fly-social.com/' },
     { title: 'Engineering', link: 'https://chitti.app/engineering/' },
     { title: 'Landing Pages', link: 'https://chitti.app/workshops' },
     { title: 'Digital It Hub', link: 'https://www.digitalithub.in/' },
@@ -13,6 +13,8 @@ const ProjectSection = () => {
     { title: 'Webinar ', link: 'https://www.buffchennai.com/campaigns/workshops/kickstarter-creative-designing-workshop' },
     { title: 'Bootcamp ', link: 'https://buffchennai.com/campaigns/bootcamps/uiux' },
     { title: 'Wordpress ', link: 'https://10web-site.ai/187/worthy-mudfish/' },
+    { title: 'Kodestory ', link: 'https://www.kodestory.com/' },
+
 
 
 
