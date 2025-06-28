d550177226e333bb272a4e9cbe4558a6f8568df9
Vinothkumar.M <102602070+vinosmart@users.noreply.github.com>
2025-06-28 10:21:24 +0530

    fix:adsense

---
+++ src/pages/_document.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8441864880126202"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
