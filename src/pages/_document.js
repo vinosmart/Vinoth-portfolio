98a79a03a2268cb64110477996a10121d0a37d0c
Vinothkumar.M <102602070+vinosmart@users.noreply.github.com>
2025-06-28 10:24:38 +0530

    fix:adsense

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,13 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google AdSense meta tag */}
+        <meta name="google-adsense-account" content="ca-pub-8441864880126202" />
+
+        {/* Google AdSense script */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8441864880126202"
